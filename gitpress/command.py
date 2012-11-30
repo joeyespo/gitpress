35844da788af3e2c007f556b3c03f5c3ae3e5214
Joe Esposito <joe@joeyespo.com>
2012-11-29 20:48:16 -0500

    Add command-line interface and 'preview' command.

--- gitpress/command.py
+++ gitpress/command.py
@@ -1,3 +1,38 @@
-def main():
+"""\
+command.py
+The command-line interface of Gitpress.
+
+Usage:
+  gitpress preview [<path>] [<address>]
+  gitpress -h | --help
+  gitpress -v | --version
+
+Where:
+  <path> is the path to the working directory of a content repository
+  <address> is what to listen on, of the form <host>[:<port>] or just <port>
+
+Options:
+  -h --help         Show this screen.
+  --version         Show version.
+"""
+
+import sys
+from docopt import docopt
+from . import full_version
+from .server import preview, resolve_address, parse_address, valid_address
+
+
+def main(initial_args=None):
     """The entry point of the application."""
-    # TODO
+    if initial_args is None:
+        initial_args = sys.argv[1:]
+    args = docopt(__doc__, argv=initial_args, version=full_version)
+
+    if args['preview']:
+        path, address = resolve_address(args['<path>'], args['<address>'])
+        if address and not valid_address(address):
+            print 'Error: Invalid address', repr(address)
+        host, port = parse_address(address)
+        return preview(working_directory=path, host=host, port=port)
+
+    return 0
