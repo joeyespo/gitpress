7bb3265eb924050fa5a6594d50423ef362fd2a48
Joe Esposito <joe@joeyespo.com>
2012-11-29 20:42:19 -0500

    Add package attributes.

--- gitpress/__init__.py
+++ gitpress/__init__.py
@@ -13,11 +13,15 @@ Links
 * `Source code <https://github.com/joeyespo/gitpress>`_
 """
 
-import command
-from helper import run
-
-
-__all__ = ['command', 'run']
-
+__title__ = 'gitpress'
 __version__ = '0.1'
+__author__ = 'Joe Esposito'
 __description__ = '\n\n'.join(__doc__.split('\n\n')[1:]).split('\n\n\n')[0]
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2012 Joe Esposito'
+
+
+from .server import preview
+from .helper import run
+
+full_version = 'Gitpress ' + __version__
