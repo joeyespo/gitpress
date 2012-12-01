8d7438f04f613e1f61460acb466607c3d8543010
Joe Esposito <joe@joeyespo.com>
2012-11-30 23:15:16 -0500

    Re-order module attributes.

--- gitpress/__init__.py
+++ gitpress/__init__.py
@@ -20,8 +20,8 @@ __title__ = 'gitpress'
 __version__ = '0.1'
 __author__ = 'Joe Esposito'
 __description__ = '\n\n'.join(__doc__.split('\n\n')[1:]).split('\n\n\n')[0]
-__license__ = 'MIT'
 __copyright__ = 'Copyright 2012 Joe Esposito'
+__license__ = 'MIT'
 
 
 from .runner import run
