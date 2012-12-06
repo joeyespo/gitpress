ccfef19014c7d310cc45fb303b4060d42faa0c1c
Joe Esposito <joe@joeyespo.com>
2012-12-06 08:20:58 -0500

    Move metadata up to setup.py.

--- gitpress/__init__.py
+++ gitpress/__init__.py
@@ -4,24 +4,11 @@ Gitpress
 
 Blissful blogging for hackers.
 
-
-Links
-`````
-
-* `Website <http://gitpress.com/>`_
-* `Documentation <http://gitpress.com/docs>`_
-* `Source code <https://github.com/joeyespo/gitpress>`_
-
 :copyright: (c) 2012 by Joe Esposito.
 :license: MIT, see LICENSE for more details.
 """
 
-__title__ = 'gitpress'
 __version__ = '0.1'
-__author__ = 'Joe Esposito'
-__description__ = '\n\n'.join(__doc__.split('\n\n')[1:]).split('\n\n\n')[0]
-__copyright__ = 'Copyright 2012 Joe Esposito'
-__license__ = 'MIT'
 
 
 from . import command
--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
+"""\
+Gitpress
+--------
+
+Blissful blogging for hackers.
+
+
+Links
+`````
+
+* `Website <http://gitpress.com/>`_
+* `Documentation <http://gitpress.com/docs>`_
+* `Source code <https://github.com/joeyespo/gitpress>`_
+
+:copyright: (c) 2012 by Joe Esposito.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1'
+__description__ = '\n\n'.join(__doc__.split('\n\n')[1:]).split('\n\n\n')[0]
+
+
 import os
 from setuptools import setup, find_packages
-import gitpress as package
 
 
 def read(fname):
@@ -9,17 +30,17 @@ def read(fname):
 
 
 setup(
-    name=package.__name__,
-    version=package.__version__,
-    description=package.__description__,
-    long_description=package.__doc__,
+    name='gitpress',
+    version=__version__,
+    description=__description__,
+    long_description=__doc__,
     author='Joe Esposito',
     author_email='joe@joeyespo.com',
     url='http://github.com/joeyespo/gitpress',
     license='MIT',
     platforms='any',
     packages=find_packages(),
-    package_data={package.__name__: ['LICENSE', 'static/*', 'templates/*']},
+    package_data={'': ['LICENSE'], 'gitpress': ['static/*', 'templates/*']},
     include_package_data=True,
     install_requires=read('requirements.txt'),
     zip_safe=False,
