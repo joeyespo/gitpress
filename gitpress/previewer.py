eb6a10f9c6b4b847b6a3f654d1c9a3679c1bc4df
Joe Esposito <joe@joeyespo.com>
2012-12-27 12:50:06 -0500

    Defaults first, then validation in previewer.

--- gitpress/previewer.py
+++ gitpress/previewer.py
@@ -5,16 +5,16 @@ from werkzeug.serving import run_simple
 def preview(working_directory=None, host=None, port=None, use_debugger=None):
     """Runs a local server to preview the working directory of a repository."""
 
-    # Validation
-    if working_directory and not os.path.isdir(working_directory):
-        raise ValueError('Directory not found: ' + repr(working_directory))
-
     # Defaults
     working_directory = os.path.abspath(working_directory or '.')
     if not host:
         host = '127.0.0.1'
     if not port:
         port = 5000
+
+    # Validation
+    if not os.path.isdir(working_directory):
+        raise ValueError('Directory not found: ' + repr(working_directory))
 
     # Configuration
     os.environ['GITPRESS_BLOG_PREVIEW'] = 'True'
