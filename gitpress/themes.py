468f9e7078cfa076e459e5e468abc1cf682c55e6
Joe Esposito <joe@joeyespo.com>
2012-12-30 03:28:10 -0500

    Cleanup themes.

--- gitpress/themes.py
+++ gitpress/themes.py
@@ -9,5 +9,5 @@ default_theme = 'default'
 def list_themes():
     """Gets a list of the installed themes."""
     repo = require_repo()
-    themes = os.path.join(repo, themes_dir)
-    return os.listdir(themes) if os.path.isdir(themes) else None
+    path = os.path.join(repo, themes_dir)
+    return os.listdir(path) if os.path.isdir(path) else None
