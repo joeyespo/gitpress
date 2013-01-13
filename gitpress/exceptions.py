e60998eec2491209eeb28ac14e8496f505beb1be
Joe Esposito <joe@joeyespo.com>
2013-01-12 21:00:04 -0500

    Add explicit message to each exception.

--- gitpress/exceptions.py
+++ gitpress/exceptions.py
@@ -5,16 +5,21 @@ class RepositoryAlreadyExistsError(Exception):
     """Indicates a Gitpress repository already exists while attempting to create
     a new one."""
     def __init__(self, content_directory, repository_directory):
-        super(RepositoryAlreadyExistsError, self).__init__()
-        self.content_directory = os.path.abspath(content_directory)
-        self.repository_directory = os.path.abspath(repository_directory)
+        content_directory = os.path.abspath(content_directory)
+        repository_directory = os.path.abspath(repository_directory)
+        super(RepositoryAlreadyExistsError, self).__init__(
+            'Gitpress repository already exists in ' + repository_directory)
+        self.content_directory = content_directory
+        self.repository_directory = repository_directory
 
 
 class RepositoryNotFoundError(Exception):
     """Indicates an existing Gitpress repository is required, but was not found."""
     def __init__(self, content_directory):
-        super(RepositoryNotFoundError, self).__init__()
-        self.directory = os.path.abspath(content_directory)
+        directory = os.path.abspath(content_directory)
+        super(RepositoryNotFoundError, self).__init__(
+            'No Gitpress repository found at ' + directory)
+        self.directory = directory
 
 
 class ConfigSchemaError(Exception):
@@ -26,13 +31,15 @@ class ConfigSchemaError(Exception):
 class ThemeNotFoundError(Exception):
     """Indicates the requested theme was not found."""
     def __init__(self, theme):
-        super(ThemeNotFoundError, self).__init__()
+        super(ThemeNotFoundError, self).__init__(
+            'Theme %s is not currently installed.' % repr(theme))
         self.theme = theme
 
 
 class NotADirectoryError(Exception):
     """Indicates a file was found when a directory was expected."""
     def __init__(self, directory, message=None):
+        directory = os.path.abspath(directory)
         super(NotADirectoryError, self).__init__(
             'Expected a directory, found a file instead at ' + directory)
-        self.directory = os.path.abspath(directory)
+        self.directory = directory
