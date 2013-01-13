9b02ec95aa75a0524af9d86e18bd7c2f741ad9b0
Joe Esposito <joe@joeyespo.com>
2013-01-13 03:20:00 -0500

    Use class-based Repository for build.

--- gitpress/building.py
+++ gitpress/building.py
@@ -1,6 +1,5 @@
 import os
-from .repositories import require_repo, presentation_files
-from .helpers import copy_files, remove_directory
+from .repository import Repository
 
 
 default_out_directory = '_site'
@@ -10,12 +9,12 @@ def build(content_directory=None, out_directory=None):
     """Builds the site from its content and presentation repository."""
     content_directory = content_directory or '.'
     out_directory = os.path.abspath(out_directory or default_out_directory)
-    repo = require_repo(content_directory)
+    repo = Repository.from_content(content_directory)
 
     # Prevent user mistakes
     if out_directory == '.':
         raise ValueError('Output directory must be different than the source directory: ' + repr(out_directory))
-    if os.path.basename(os.path.relpath(out_directory, content_directory)) == '..':
+    if os.path.basename(os.path.relpath(out_directory, repo.content_directory)) == '..':
         raise ValueError('Output directory must not contain the source directory: ' + repr(out_directory))
 
     # TODO: read config
@@ -23,9 +22,6 @@ def build(content_directory=None, out_directory=None):
     # TODO: init and run plugins
     # TODO: process with active theme
 
-    # Collect and copy static files
-    files = presentation_files(repo)
-    remove_directory(out_directory)
-    copy_files(files, out_directory, repo)
+    # TODO: Collect and copy files
 
     return out_directory
