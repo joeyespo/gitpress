ab4f5ad0544c20124ed51fac9834cd6ad9de8878
Joe Esposito <joe@joeyespo.com>
2013-01-13 03:42:28 -0500

    Bugfix: Add missing template resolver function.

---
+++ gitpress/templates/__init__.py
@@ -0,0 +1,28 @@
+"""\
+gitpress.templates
+~~~~~~~~~~~~~~~~~~
+
+Defines the built-in templates.
+"""
+
+import os
+
+
+templates_directory = os.path.dirname(__file__)
+default_template = 'default'
+builtin_templates = [
+    'default',
+]
+
+
+def resolve_template(name=None):
+    """\
+    Returns the path to the specified template name.
+    Note that name can also be an absolute path.
+    If a template is not found or the path does not exist, None is returned.
+    """
+    if name is None:
+        name = default_template
+    if not os.path.isabs(name):
+        name = os.path.join(templates_directory, name)
+    return name if os.path.exists(name) else None
